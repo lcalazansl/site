8d595bf1b9ebd92d9001c63fc26e210747e59fe3
Lucas Luz <lucas@calazansluz.com>
2022-10-04 13:42:23 +0200

    sass dir org

---
+++ scripts/gtag.js
@@ -0,0 +1,7 @@
+
+  // <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZRB7LN5YR9"></script>
+
+  //     window.dataLayer = window.dataLayer || [];
+  //     function gtag(){dataLayer.push(arguments);}
+  //     gtag('js', new Date());
+  //     gtag('config', 'G-ZRB7LN5YR9');
